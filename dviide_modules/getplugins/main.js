8f360fb22122eab3aa130af949a84a96964f647e
Vincent Brouwer <kipsensatie@gmail.com>
2017-06-09 23:46:12 +0200

    Updated getplugins for better response

--- dviide_modules/getplugins/main.js
+++ dviide_modules/getplugins/main.js
@@ -4,5 +4,9 @@ this.start = function() {
   for(i=0;i<navigator.plugins.length;i++) {
     temppl += i.toString() + '. ' + navigator.plugins[i].name + '\n';
   }
-  dviide.callback(temppl);
+  if(temppl != '') {
+    dviide.callback(temppl);
+  } else {
+    dviide.callback('No plugins found!');
+  }
 }
